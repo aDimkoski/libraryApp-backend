20f6650be1ed580af52f87cba6c123d3dfa5bef8
Aleksandar <alekmicky@gmail.com>
2022-04-04 22:18:55 +0200

    fixed cross origin

--- src/main/java/com/emt/libraryappemt/web/rest/CountryRestController.java
+++ src/main/java/com/emt/libraryappemt/web/rest/CountryRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://libraryapp-frontend.herokuapp.com")
 @RequestMapping("/api/countries")
 public class CountryRestController {
 
